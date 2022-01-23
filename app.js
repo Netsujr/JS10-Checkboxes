872309c489a28f96f054c8d0ded20a642bdbc7ed
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-23 14:22:19 +0900

    holding shift works, but finding all checkboxes

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 let lastChecked;
 
 function handleCheck(event) {
-  // console.log(event);
+  console.log(event);
+  let inBetween = false
   if(event.shiftKey && this.checked) {
-
+checkboxes.forEach(checkbox => {
+  console.log(checkbox);
+});
   }
   lastChecked = this;
   console.log(lastChecked);
