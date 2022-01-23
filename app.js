452c5b45bcedd9a42bf4cf2f9f729c58e50dfda3
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-23 14:48:43 +0900

    Works! but getting a bug on original input

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+const checkAll = document.querySelector('.special input[type="checkbox"]');
+
 // console.log(checkboxes);
+console.log(checkAll);
+
+function checkAllBoxes() {
+  if (checkAll.checked) {
+    checkboxes.forEach(checkbox => {
+      checkbox.checked = !checkbox.checked;
+    });
+    console.log(checkbox);
+  }
+}
 
 let lastChecked;
 
 function handleCheck(event) {
-  console.log(event);
+
+  // console.log(event);
   let inBetween = false;
   if(event.shiftKey && this.checked) {
     checkboxes.forEach(checkbox => {
-      console.log(checkbox);
+      // console.log(checkbox);
       if(checkbox === this || checkbox === lastChecked) {
         inBetween = !inBetween;
         console.log('Between Here?');
@@ -20,8 +33,9 @@ function handleCheck(event) {
     });
   }
   lastChecked = this;
-  console.log(lastChecked);
+  // console.log(lastChecked);
 }
 
 
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+checkAll.addEventListener('click', checkAllBoxes);
